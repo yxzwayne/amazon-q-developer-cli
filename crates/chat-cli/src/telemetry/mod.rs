aa84de05d54b4dc0d68154ce5951ad582c6cb5c0
kkashilk <93673379+kkashilk@users.noreply.github.com>
2025-08-25 20:07:14 -0700

    Short-Term fix for SendTelemetry API Validation errors (#2694)

--- crates/chat-cli/src/telemetry/mod.rs
+++ crates/chat-cli/src/telemetry/mod.rs
@@ -572,12 +572,17 @@ impl TelemetryClient {
         } = &event.ty
         {
             let user_context = self.user_context().unwrap();
+            // Short-Term fix for Validation errors -
+            // chatAddMessageEvent.timeBetweenChunks' : Member must have length less than or equal to 100
+            let time_between_chunks_truncated = time_between_chunks_ms
+                .as_ref()
+                .map(|chunks| chunks.iter().take(100).cloned().collect());
 
             let chat_add_message_event = match ChatAddMessageEvent::builder()
                 .conversation_id(conversation_id)
                 .message_id(message_id.clone().unwrap_or("not_set".to_string()))
                 .set_time_to_first_chunk_milliseconds(*time_to_first_chunk_ms)
-                .set_time_between_chunks(time_between_chunks_ms.clone())
+                .set_time_between_chunks(time_between_chunks_truncated)
                 .set_response_length(*assistant_response_length)
                 .build()
             {
